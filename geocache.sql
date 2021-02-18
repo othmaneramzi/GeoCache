8852193811efb79be35a1d58976a70d5ee606400
othmaneramzi <37915365+othmaneramzi@users.noreply.github.com>
2021-02-18 16:58:01 +0100

    Add files via upload

---
+++ geocache.sql
@@ -0,0 +1,141 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:8889
+-- Generation Time: Feb 18, 2021 at 03:54 PM
+-- Server version: 5.7.26
+-- PHP Version: 7.3.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+--
+-- Database: `geocache`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cache`
+--
+
+CREATE TABLE `cache` (
+  `idCache` bigint(20) NOT NULL,
+  `coordonnees` varchar(20) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `etat` varchar(20) NOT NULL,
+  `nature` varchar(20) NOT NULL,
+  `type` varchar(20) NOT NULL,
+  `lieuId` bigint(20) DEFAULT NULL,
+  `idUser` bigint(20) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `lieu`
+--
+
+CREATE TABLE `lieu` (
+  `idLieu` bigint(20) NOT NULL,
+  `nomLieu` varchar(20) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `utilisateur`
+--
+
+CREATE TABLE `utilisateur` (
+  `idUtilisateur` bigint(20) NOT NULL,
+  `description` varchar(20) NOT NULL,
+  `nom` varchar(20) NOT NULL,
+  `pseudo` varchar(20) NOT NULL,
+  `type` varchar(20) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `utilisateur`
+--
+
+INSERT INTO `utilisateur` (`idUtilisateur`, `description`, `nom`, `pseudo`, `type`) VALUES
+(1, 'fan du jeu', 'Othmane RAMZI', 'KiseRyota', 'proprietaire'),
+(2, 'fan du jeu', 'Othmane RAMZI', 'KiseRyota', 'proprietaire'),
+(3, 'joueur', 'Pierre DUPONT', 'Dupont', 'visiteur');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `visite`
+--
+
+CREATE TABLE `visite` (
+  `idVisite` bigint(20) NOT NULL,
+  `commentaire` varchar(20) DEFAULT NULL,
+  `date` datetime NOT NULL,
+  `logCache` varchar(20) DEFAULT NULL,
+  `cacheID` bigint(20) DEFAULT NULL,
+  `utilsateurId` bigint(20) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `cache`
+--
+ALTER TABLE `cache`
+  ADD PRIMARY KEY (`idCache`),
+  ADD KEY `FK3ktecht12g53gv9cptm8ylh30` (`lieuId`),
+  ADD KEY `FKci1f5u92oc4t1lctbaw7pdffj` (`idUser`);
+
+--
+-- Indexes for table `lieu`
+--
+ALTER TABLE `lieu`
+  ADD PRIMARY KEY (`idLieu`);
+
+--
+-- Indexes for table `utilisateur`
+--
+ALTER TABLE `utilisateur`
+  ADD PRIMARY KEY (`idUtilisateur`);
+
+--
+-- Indexes for table `visite`
+--
+ALTER TABLE `visite`
+  ADD PRIMARY KEY (`idVisite`),
+  ADD KEY `FKkm4t00d2t7s0hh1n4ywummrlb` (`cacheID`),
+  ADD KEY `FKayls98egt1xsjfln6qilgac4q` (`utilsateurId`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `cache`
+--
+ALTER TABLE `cache`
+  MODIFY `idCache` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `lieu`
+--
+ALTER TABLE `lieu`
+  MODIFY `idLieu` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `utilisateur`
+--
+ALTER TABLE `utilisateur`
+  MODIFY `idUtilisateur` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `visite`
+--
+ALTER TABLE `visite`
+  MODIFY `idVisite` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
